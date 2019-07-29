a1a58de985ed26662fa6d162e51352d6cde1d02a
Satadru Pramanik <satadru@umich.edu>
2019-07-29 15:54:30 -0400

    update compress cmd

--- build-rpi4.sh
+++ build-rpi4.sh
@@ -595,11 +595,13 @@ export_compressed_image () {
      echo "* Compressing ${new_image} with $i and exporting"
      echo "  out of container to:"
      echo "${new_image}-${KERNEL_VERSION}_${now}.img.$i"
-     compresscmd="$i -v -k ${new_image}.img && \
-     mv ${new_image}.img.$i \
+     compresscmd="$i -v -k ${new_image}.img
+     cpcmd="cp /build/source/${new_image}.img.$i \
      /output/${new_image}-${KERNEL_VERSION}_${now}.img.$i"
-     #echo $compresscmd
+     echo $compresscmd
      $compresscmd
+     echo $cpcmd
+     $cpcmd
      chown $USER:$GROUP /output/${new_image}-${KERNEL_VERSION}_${now}.img.$i
      echo "/output/${new_image}-${KERNEL_VERSION}_${now}.img.$i created."
     done
