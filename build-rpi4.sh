f45e45f585c47ac55fb7a8202653b947185f765a
Satadru Pramanik <satadru@umich.edu>
2019-07-29 17:01:55 -0400

    remove xz from default compression

--- build-rpi4.sh
+++ build-rpi4.sh
@@ -19,7 +19,8 @@ new_image="eoan-preinstalled-server-arm64+raspi4"
 # Note that these only work for the chroot commands.
 silence_apt_flags="-o Dpkg::Use-Pty=0 -qq < /dev/null > /dev/null "
 silence_apt_update_flags="-o Dpkg::Use-Pty=0 < /dev/null > /dev/null "
-image_compressors=("lz4" "xz")
+#image_compressors=("lz4" "xz")
+image_compressors=("lz4")
 
 
 # Set Time Stamp
