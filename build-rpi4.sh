830a83573d33075074c8073c5d9857b268c9c87b
Satadru Pramanik <satadru@umich.edu>
2019-08-02 17:21:58 -0400

    remove missing function

--- build-rpi4.sh
+++ build-rpi4.sh
@@ -543,8 +543,6 @@ startfunc
     cp $apt_cache/linux-image-$KERNEL_VERSION_$KERNEL_VERSION-1_arm64.deb $workdir/
     cp $apt_cache/linux-headers-$KERNEL_VERSION_$KERNEL_VERSION-1_arm64.deb $workdir/
     else
-   
-        mk_kernel_debs
         echo "* Making $KERNEL_VERSION kernel debs."
         cd $workdir/rpi-linux
         debcmd="make \
